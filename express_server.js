b03b3400560704ff96f3fb7dbd765c67f1442034
Jonas Cura <jonasm.cura@gmail.com.com>
2023-05-31 16:38:39 -0600

    Add get /urls/new and random string function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateRandomString() {
+  let result = '';
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    // code referenced from https://www.programiz.com/javascript/examples/generate-random-strings
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+console.log(generateRandomString());
 
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +27,10 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -29,6 +48,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
