a43e0d095b3fc990021628a52330484d57d71d75
Jonas Cura <jonasm.cura@gmail.com.com>
2023-06-02 01:27:56 -0600

    Add functional register app.post

--- express_server.js
+++ express_server.js
@@ -7,6 +7,25 @@ const app = express();
 const PORT = 8080; // default port 8080
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+// data
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  abc: {
+    id: "abc",
+    email: "user@example.com",
+    password: "123",
+  },
+  def: {
+    id: "def",
+    email: "user2@example.com",
+    password: "456",
+  },
+};
+
 // configuration
 app.set("view engine", "ejs");
 
@@ -15,6 +34,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// string function for urlDatabase
 function generateRandomString() {
   let result = '';
   const length = 6;
@@ -23,11 +43,6 @@ function generateRandomString() {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
-};
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
 };
 
 // CREATE: urls page
@@ -112,8 +127,22 @@ app.post("/urls/:id", (req, res) => {
 
 // SAVE: login
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
+  console.log(req.body);
+  const id = req.body.username;
+  let foundUser = null;
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.id === id) {
+      // user found
+      foundUser = user;
+    }
+  }
+
+  if (!foundUser) {
+    return res.status(400).send("no user with that name found");
+  } else {
+    res.cookie("username", id)
+  }
   console.log(req.cookies);
   res.redirect("/urls");
 });
@@ -132,7 +161,52 @@ app.get("/register", (req, res) => {
 
 //SAVE: registration
 app.post("/register", (req, res) => {
-  res.redirect("/urls");
+  // grab info from body
+  const email = req.body.email;
+  const password = req.body.password;
+  // console.log(`email: ${email} password: ${password}`)
+
+  if (!email || !password) {
+    return res.status(400). send("you must provide a username and password");
+  }
+
+  // look for existing email
+  let foundUser = null;
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      // email already exists
+      foundUser = user;
+    }
+  }
+  console.log("before users", users)
+  console.log(req.body)
+  
+  if (!foundUser) {
+    const id = {
+      id: generateRandomString()
+    };
+    users[id.id] = id;
+    users[id.id].email = email;
+    users[id.id].password = password;
+    res.cookie("username", id.id);
+  } else {
+    return res.status(400).send("email already registered");
+  }
+
+  console.log("after users", users)
+
+  
+  res.redirect('/urls');
+
+  /* login logic
+  // lookup user
+  
+
+  if (foundUser. password !== password) {
+    return res.status(400).send("passwords do not match");
+  }
+  */
 });
 
 app.listen(PORT, () => {
